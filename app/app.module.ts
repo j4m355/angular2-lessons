791b59bacd34fc7ed1487f08f24b9620108425fa
James Campbell <james@Jamess-MacBook-Pro.local>
2016-08-29 23:54:59 +0100

    6-1

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes-component';
+import { HeroService } from './hero.service';
 
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
